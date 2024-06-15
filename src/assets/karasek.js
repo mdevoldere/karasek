308b3277c17294ec7d2db550914681c00c36b3e8
MDevoldere <mdevoldere@users.noreply.github.com>
2024-06-15 10:34:23 +0200

    add form

--- src/assets/karasek.js
+++ src/assets/karasek.js
@@ -2,7 +2,9 @@ class KarasekQuestion
 {
     constructor(question) {
         Object.assign(this, question);
+        this.score = 0;
     }
 }
+
 
 export { KarasekQuestion }
--- src/assets/main.js
+++ src/assets/main.js
@@ -3,14 +3,38 @@ import { KarasekQuestion } from "./karasek.js";
 const karasekApp = {
     data() {
         return {
-
+            services: ['RF', 'OFP'],
+            answersLabels: [
+                { score: 1, label: "Pas du tout d'accord"},
+                { score: 2, label: "Pas d'accord"},
+                { score: 3, label: "D'accord"},
+                { score: 4, label: "Tout à fait d'accord"}
+            ],
+            selectedService: '',
+            questionsSrc: [],
+            questions: []
         }
     },
     async mounted() {
-
+        let r = await fetch('./assets/karasek.json');
+        this.questionsSrc = await r.json();
+        for(let q of this.questionsSrc) {
+            this.questions.push(new KarasekQuestion(q));
+        }
+        console.log(this.questionsSrc);
+    },
+    computed: {
+        hasSelectedService() {
+            return this.selectedService.length > 1;
+        }
     },
     methods: {
-
+        check() {
+            console.log(this.questions);
+        },
+        selectService(event) {
+            //this.selectedService = event.target.
+        }
     }
 }
 
