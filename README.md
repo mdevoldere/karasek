b11beea345bc8f99c5fdfc3b7c64a52c592825ff
MDevoldere <mdevoldere@users.noreply.github.com>
2024-06-18 14:16:20 +0200

    add calc

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # karasek
 
+Le questionnaire de Karasek permet une évaluation des facteurs psychosociaux au travail. Ce questionnaire comporte 3 dimensions distinctes :
+
+- La charge psychologique de travail perçue par le travailleur, de manière aussi bien qualitative que quantitative,
+- La latitude décisionnelle :
+    - Avec la possibilité d’utiliser et de développer ses compétences et/ou ses qualifications
+    - Et l’autonomie décisionnelle, autant dans la manière de faire son travail que dans les décisions qui s’y rattachent.
+- Le soutien socio-émotionnel au travail de la part des collègues et des supérieurs du travailleur.
+
+Le modèle de Karasek a ensuite été développé par Theorell en 1990, en effet, les individus peuvent être classés dans une matrice afin de connaitre leur niveau de stress au travail.
+
+--- 
+
+Il n’y a que 4 réponses possibles pour chaque question :
+
+- Pas du tout d’accord (1)
+- Pas d’accord (2)
+- D’accord (3)
+- Tout à fait d’accord (4)
+
+Les valeurs entre parenthèses vous permettront de calculer le score final. 
+
+Il est calculé de la façon suivante :
+
+- Autonomie = 4 x [Q4 + (5-Q6) + Q8]
+- Utilisation des compétences = [Q1 + (5-Q2) + Q3 + Q5 +Q7 + Q9]
+- Latitude décisionnelle = Autonomie + Utilisation des compétences
+- Demande psychologique = Q10 + Q11 + Q12+( 5-Q13) + Q14 + Q15 + Q16+ Q17 + Q18
+- Soutien social = Q23 + Q24 + Q25+ Q26
+
--- src/assets/karasek.js
+++ src/assets/karasek.js
@@ -6,5 +6,69 @@ class KarasekQuestion
     }
 }
 
+class KarasekCalc 
+{
+    constructor(questions) {
+        this.q = questions;
+    }
 
-export { KarasekQuestion }
+    getAutonomie() {
+        // Autonomie = 4 x [Q4 + (5-Q6) + Q8]
+        let q4 = this.q[3].score;
+        let q6 = this.q[5].score;
+        let q8 = this.q[7].score;
+
+        return (4 * (q4 + (5-q6) + q8));
+    }
+
+    getUtilisationCompetences() {
+        // Utilisation des compétences = 
+        // [Q1 + (5-Q2) + Q3 + Q5 +Q7 + Q9]
+        let q1 = this.q[0].score;
+        let q2 = this.q[1].score;
+        let q3 = this.q[2].score;
+        let q5 = this.q[4].score;
+        let q7 = this.q[6].score;
+        let q9 = this.q[8].score;
+
+        return (q1 + (5-q2) + q3 + q5 + q7 + q9);
+    }
+
+    getAutonomieDecisionnelle() {
+        // Latitude décisionnelle = 
+        // Autonomie + Utilisation des compétences
+        return this.getAutonomie() + this.getUtilisationCompetences();
+    }
+
+    getDemandePsychologique() {
+        // Demande psychologique = 
+        // Q10 + Q11 + Q12+( 5-Q13) + Q14 + Q15 + Q16+ Q17 + Q18
+        
+        let q10 = this.q[9].score;
+        let q11 = this.q[10].score;
+        let q12 = this.q[11].score;
+        let q13 = this.q[12].score;
+        let q14 = this.q[13].score;
+        let q15 = this.q[14].score;
+        let q16 = this.q[15].score;
+        let q17 = this.q[16].score;
+        let q18 = this.q[17].score;
+
+        return (q10 + q11 + q12 + (5 - q13) + q14 + q15 + Q16 + q17 + q18);
+    }
+
+    getSoutienSocial() {
+        // Soutien social = Q23 + Q24 + Q25+ Q26
+        
+        let q23 = this.q[22].score;
+        let q24 = this.q[23].score;
+        let q25 = this.[q24].score;
+        let q26 = this.q[25].score;
+
+        return (q23 + q24 + q25 + q26);
+    }
+
+
+}
+
+export { KarasekQuestion, KarasekCalc }
